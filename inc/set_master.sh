840c2fe044c8c6a6cbd88a9e566b7d244e4d57fa
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 05:40:24 +0600

    move to case

--- inc/set_master.sh
+++ inc/set_master.sh
@@ -2,13 +2,26 @@
 # set_master
 function set_master {
   HOSTSFILE="/etc/hosts"
-  if grep -Fxqs "$HOSTSFILE" puppet
-  then
+  # if grep -Fxqs "$HOSTSFILE" puppet
+  # then
+  #   # code if found
+  #   echo 'Already in place'
+  # else
+  #   # code if not found
+  #   echo '192.168.56.2 puppet' | sudo tee -a $HOSTSFILE
+  # fi
+  case `grep -Fx "$HOSTSFILE" "puppet" >/dev/null; echo $?` in
+  0)
     # code if found
     echo 'Already in place'
-  else
+    ;;
+  1)
     # code if not found
     echo '192.168.56.2 puppet' | sudo tee -a $HOSTSFILE
-  fi
+    ;;
+  *)
+    # code if an error occurred
+    ;;
+  esac
   ping -c 2 puppet
 }
