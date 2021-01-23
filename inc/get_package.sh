1e7ea1af5840d48c33cfe14ebf0e7dbe7b882cb6
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 05:14:34 +0600

    added sudo

--- inc/get_package.sh
+++ inc/get_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function get_package {
   package='puppet5-release-bionic.deb'
-  wget https://apt.puppetlabs.com/${package} -o /tmp/${package}
+  sudo wget https://apt.puppetlabs.com/${package} -o /tmp/${package}
   sudo dpkg -i /tmp/${package}
 }
