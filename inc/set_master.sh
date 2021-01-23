db98a2fafa118263a69442d9d7a0cb1e25c42a9d
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 05:26:30 +0600

    set_master.sh added

---
+++ inc/set_master.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# set_master
+function set_master {
+  echo '192.168.56.2 puppet' | sudo tee -a /etc/hosts
+  ping -c 2
+}
--- main.sh
+++ main.sh
@@ -4,9 +4,13 @@
 # edit hosts file
 # download package agent -> install package agent -> install puppet module
 #                        -> add to sudoers -> sign certificate -> set run interval -> start service
+source inc/set_master.sh
 source inc/get_package.sh
 
 echo "Started installing Puppet"
 
+# adding a master host
+set_master
+
 # download package agent
-get_package
+#get_package
