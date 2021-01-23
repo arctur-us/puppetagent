fbcd87cb6f8ed19e129d1b529309834c07158305
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 05:48:48 +0600

    fix if

--- inc/set_master.sh
+++ inc/set_master.sh
@@ -9,7 +9,7 @@ function set_master {
   else
     # code if not found
     echo '192.168.56.2 puppet' | sudo tee -a $HOSTSFILE
-  # fi
+  fi
   # case `grep -Fx puppet "$HOSTSFILE" >/dev/null; echo $?` in
   # 0)
   #   # code if not found
