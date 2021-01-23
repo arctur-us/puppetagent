0237b940ea5a08467b1f56cb6e43319f32c88a75
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 05:58:46 +0600

    final set_master

--- inc/set_master.sh
+++ inc/set_master.sh
@@ -5,24 +5,11 @@ function set_master {
   if grep -Fq puppet "$HOSTSFILE"
   then
     # code if found
-    echo 'Already in place'
+    ;
   else
     # code if not found
     echo | sudo tee -a $HOSTSFILE
     echo '192.168.56.2 puppet' | sudo tee -a $HOSTSFILE
   fi
-  # case `grep -Fx puppet "$HOSTSFILE" >/dev/null; echo $?` in
-  # 0)
-  #   # code if not found
-  #   echo '192.168.56.2 puppet' | sudo tee -a $HOSTSFILE
-  #   ;;
-  # 1)
-  #   # code if found
-  #   echo 'Already in place'
-  #   ;;
-  # *)
-  #   # code if an error occurred
-  #   ;;
-  # esac
-  ping -c 2 puppet
+  echo 'Master is defined'
 }
