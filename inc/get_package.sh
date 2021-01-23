abc1bcab2710cc45fca08cadab4d2ea3eae74776
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 04:48:05 +0600

    wget fix

--- inc/get_package.sh
+++ inc/get_package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 function get_package {
-  wget https://apt.puppetlabs.com/puppet5-release-bionic.deb
+  wget https://apt.puppetlabs.com/puppet5-release-bionic.deb /tmp/puppet5-release-bionic.deb
 }
