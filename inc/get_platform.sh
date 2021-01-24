194fdf62a640d6a5fe8a5bb516d6a621569d0ae0
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-25 01:21:21 +0600

    os cases

--- inc/get_platform.sh
+++ inc/get_platform.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 function get_platform {
-  echo $OSTYPE
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    linux=`cat /etc/os-release | grep ^ID=`
+    if [[ "$linux" == "fedora" ]]; then
+      echo "Red Hat"
+    elif [[ "$linux" == "ubuntu" ]]; then
+      echo "Debian"
+    fi
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Darwin"
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
+    # POSIX compatibility layer and Linux environment emulation for Windows
+    echo "Emu"
+  elif [[ "$OSTYPE" == "msys" ]]; then
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    echo "Mingw"
+  elif [[ "$OSTYPE" == "win32" ]]; then
+    # I'm not sure this can happen.
+    echo "Win"
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    echo "Freebsd"
+  else
+    echo "OS not supported"
+  fi
 }
