4c1c830d0ce8dcd536668870d6fe761c698b20c2
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 16:04:29 +0600

    added check for package

--- inc/get_package.sh
+++ inc/get_package.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 function get_package {
   package='puppet5-release-bionic.deb'
-  sudo wget -O /tmp/${package} https://apt.puppetlabs.com/${package}
-  sudo dpkg -i /tmp/${package}
+  # need a check if installed
+  if ! dpkg -l | grep puppet
+  then
+    # install package
+    sudo wget -O /tmp/${package} https://apt.puppetlabs.com/${package}
+    sudo dpkg -i /tmp/${package}
+  fi
 }
