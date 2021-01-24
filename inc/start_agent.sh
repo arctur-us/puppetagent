5ee9d284974c00aba3c9df28dce51d34d7b626a1
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 21:39:39 +0600

    test secure_path

--- inc/start_agent.sh
+++ inc/start_agent.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # start_agent.sh
 function start_agent {
+  SUDOERS="/root/sudoers"
+  if grep -c puppet "$SUDOERS"
+  then
+    # if found 0 matches
+    chmod 640 $SUDOERS
+    search='secure_path="'
+    replace='secure_path="/opt/puppetlabs/bin:'
+    sed -i "s/$search/$replace/" $filename
+    chmod 440 $SUDOERS
+  fi
+
   # This should go to j2 template in the future
   CONFFILE="/etc/puppetlabs/puppet/puppet.conf"
   if ! grep -Fq master "$CONFFILE"
@@ -18,6 +29,6 @@ function start_agent {
   echo 'Added to systemd'
 
   # check if already running and enabled
-  sudo systemctl restart puppet
-  sudo systemctl enable puppet
+#  sudo systemctl restart puppet
+#  sudo systemctl enable puppet
 }
