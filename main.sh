b55f7bf590f5f5ed5764b4c8d485c56604121585
Yerbol Khassen <yerbolkhassen@gmail.com>
2021-01-24 17:03:08 +0600

    fix inc

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 # download package agent -> install package agent -> install puppet module
 #                        -> add to sudoers -> sign certificate -> set run interval -> start service
 # include additional sources
-source inc/*
+source <(cat inc/*)
 # source inc/set_master.sh
 # source inc/get_package.sh
 # source inc/install_package.sh
